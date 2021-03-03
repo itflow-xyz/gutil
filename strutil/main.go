4d1aee286fd109d08463cafd59a585cf9a98b459
giuliobosco <giuliobva@gmail.com>
2021-03-03 19:32:19 +0100

    add strutil.ContainsOnlyLetter()

--- strutil/main.go
+++ strutil/main.go
@@ -1,5 +1,9 @@
 package strutil
 
+import (
+	"github.com/bomaidea/gutil/charutil"
+)
+
 // Reverse reverts a string ASD -> DSA, abc -> cba
 func Reverse(s string) string {
 	var out string
@@ -9,4 +13,15 @@ func Reverse(s string) string {
 	}
 
 	return out
+}
+
+// ContainsOnlyLetter returns true if the string
+// given as parameter contains only strings
+func ContainsOnlyLetter(s string) bool {
+	for _, v := range s {
+		if !charutil.IsLetter(v) {
+			return false
+		}
+	}
+	return true
 }
