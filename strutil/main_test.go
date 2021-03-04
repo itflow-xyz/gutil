ffe15e13ba4f9641cd2b96e46e571a3f63bfb768
giuliobosco <giuliobva@gmail.com>
2021-03-04 13:29:42 +0100

    fix strutil testing

--- strutil/main_test.go
+++ strutil/main_test.go
@@ -3,6 +3,7 @@ package strutil
 import (
 	"testing"
 
+	"github.com/bomaidea/gutil/charutil"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,4 +11,12 @@ func TestReverse(t *testing.T) {
 	assert.Equal(t, "AA", Reverse("AA"))
 	assert.Equal(t, "CBA", Reverse("ABC"))
 	assert.Equal(t, "ABA", Reverse("ABA"))
+}
+
+func TestContainsOnlyLetter(t *testing.T) {
+	assert.True(t, ContainsOnlyLetter(charutil.LettersUppercase))
+	assert.True(t, ContainsOnlyLetter(charutil.LettersLowercase))
+
+	assert.False(t, ContainsOnlyLetter(charutil.Numbers))
+	assert.False(t, ContainsOnlyLetter(charutil.SpecialChars))
 }
